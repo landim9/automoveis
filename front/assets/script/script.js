2f2acba7d3c288fe308f687b6efe9474198955a8
landim9 <landimr9des@gmail.com>
2024-10-15 11:20:12 -0300

    Atts

--- front/assets/script/script.js
+++ front/assets/script/script.js
@@ -1,7 +1,53 @@
-const uri = 'http://localhost:3000/'
+const uri = 'http://localhost:3000/';
+const divs = document.querySelectorAll('.quad');
+const corpo = document.getElementById('corpo');
 
-const quad = document.querySelectorAll('.quad')
-const area = document.getElementById('area')
+function formatarPreco(preco) {
+    return 'R$ ' + preco.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+}
 
-area.innerHTML = 'oaaaaaaaa'
+divs.forEach(div => {
+    div.addEventListener('click', () => {
+        const conteudo = div.textContent;
+
+        document.getElementById('area').textContent = 'Área ' + conteudo;
+
+        fetch(uri + 'alocacao/' + conteudo)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('woNetrk response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data && Array.isArray(data)) {
+                    corpo.innerHTML = '';
+                    data.forEach(aloc => {
+                        corpo.innerHTML += `
+                        <tr>
+                            <td style="text-align:center">${aloc.automoveis.Modelo}</td>
+                            <td id="preco">${formatarPreco(aloc.automoveis.Preco)}</td>
+                            <td style="text-align:center">
+                                <button class="bexcluir" onclick="excluir(${aloc.id})">Excluir</button>
+                                <button class="bvisualizar" onclick="visualizar(${aloc.id})">Visualizar</button>
+                            </td>
+                        </tr>`;
+                    });
+                } else {
+                    console.warn('Nenhuma alocação encontrada ou a estrutura é inválida.');
+                }
+            })
+            .catch(error => {
+                console.error('Erro ao buscar alocação:', error);
+            });
+    });
+});
+
+
+
+
+function modal() {
+    modalNovo.classList.remove('oculto')
+}
+
 
